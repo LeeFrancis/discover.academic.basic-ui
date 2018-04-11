c9b785a74e0aaba02ccd32e6013b729292542b87
Lee Francis <lfrancis@ebsco.com>
2018-04-11 15:21:21 -0400

    Initial Commit

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+import { routerReducer as router } from 'react-router-redux';
+import theme from './theme';
+import locale from './locale';
+import basicResearcherReducers from '../micro-uis/landing/reducers';
+
+export default combineReducers(Object.assign({ router, theme, locale }, basicResearcherReducers));
